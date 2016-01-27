d396a422a6c4c27d1e614a8e89421614fcdddf62
cdhekne <chinmaydhekne@gmail.com>
2016-01-27 00:28:55 -0700

    Adding logic

--- Thesis_WebApp/WebContent/results.jsp
+++ Thesis_WebApp/WebContent/results.jsp
@@ -1,23 +1,36 @@
 <%@ page language="java" contentType="text/html; charset=US-ASCII"
-    pageEncoding="US-ASCII"%>
+	pageEncoding="US-ASCII"%>
+	
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
+
 <title>Insert title here</title>
 </head>
-<body>
-	<%-- <%
-	Course courses = (Course) request.getAttribute("courses");
-	%> --%>
-	
-	<!-- Identify variable in the id parameter, type in class, scope in scope 
-	Code between tags will be executed after object creation -->
-	<jsp:useBean id="courses" class="com.ml.course.Course" scope="request">
-		<jsp:setProperty property="courses" name="courses" value="NewCourses" />
-	</jsp:useBean>
-	
-	<!-- JSTL calls get method implicitly -->
-	<p>Hello <b><jsp:getProperty property="courses" name="courses" /></b>!</p>
+<body >
+	<table class="table">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Duration</th>
+			</tr>
+		</thead>
+		
+			
+	</table>
+	<script>
+	function foo(){
+		
+		alert("asasd");
+		var json = request.getAttribute("json");
+		alert(json);
+	}
+</script>
+<%
+    Object value = request.getAttribute("json");
+%>
+
+<p><%=value%></p>
 </body>
 </html>
--- Thesis_WebApp/src/com/ml/search/SearchServlet.java
+++ Thesis_WebApp/src/com/ml/search/SearchServlet.java
@@ -1,6 +1,7 @@
 package com.ml.search;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,9 +15,12 @@ import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFNode;
 
-import com.ml.course.Course;
+import com.google.gson.Gson;
 import com.ml.query.QueryInfo;
 
 /**
@@ -24,8 +28,8 @@ import com.ml.query.QueryInfo;
  */
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String url = "http://sebk.me/mooc_7_1.rdf";
-	private static final String db = "http://sebk.me:3030/ds/query";
+	private static final String url = "/home/cdhekne/Documents/Thesis/udacity_1.ttl";
+	private static final String db = "http://localhost:3030/udacity/query";
 	private final OntModel m = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM, null);
 	private QueryInfo qi = new QueryInfo();
 	
@@ -34,36 +38,79 @@ public class SearchServlet extends HttpServlet {
 		
 		keyword = request.getParameter("keyword");
 		
-		if(keyword.isEmpty()) {
+		/*if(keyword.isEmpty()) {
 			// Send back to home page
 			response.sendRedirect("index.jsp");
 			return;
 		}
-		else {
+		else {*/
 			m.read(url, "RDF/XML");
 			
-			String queryString = "PREFIX mooc: <http://sebk.me/MOOC.owl#>\n" +
+			String queryString = "prefix edu: <http://cdhekne.github.io/mooc.owl#>\n" +
 				"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
 				"PREFIX schema: <http://schema.org/>\n" +
-				"SELECT * WHERE {\n" +
-		        "?course rdf:type mooc:Course.\n" +
-		     	"?course schema:name ?iname.\n" +
-		        "FILTER (regex(?iname, \"" + keyword + "\", \"i\")).\n" +
+				"SELECT ?Short_Name ?Duration WHERE {\n" +
+		        "?course edu:Duration ?Duration ; edu:Short_Name ?Short_Name .\n" +
+		        "FILTER (regex(?Short_Name, \"" + keyword + "\", \"i\")).\n" +
 				"}";
 			
 			Query query = QueryFactory.create(queryString) ;
 			QueryExecution qexec = QueryExecutionFactory.sparqlService(db, query);
-			Course courses = new Course();
+			
+			/*Course courses = new Course();
 			courses.setCourses(qi.getCourses(qexec));
 			request.setAttribute("courses", courses);
+			*/
 			
 			// Better: forward request & response with redirect
 			// Browser doesn't know that this is a new request, request
 			// and response parameters are carried over
-			RequestDispatcher dispatcher = request.getRequestDispatcher("results.jsp");
-			dispatcher.forward(request, response);
+			String json="";
+			try{
+				
+				ResultSet responseResultSet = qexec.execSelect();
+				while( responseResultSet.hasNext())
+				{
+					HashMap<String, String> j = new HashMap<String, String>();
+					Gson gson = new Gson();
+					QuerySolution soln = responseResultSet.nextSolution();
+					RDFNode name = soln.get("?Short_Name");
+					RDFNode recBackground = soln.get("?Duration");
+					j.put("name", name.toString());
+					j.put("Duration", recBackground.toString());
+					json += "\n"+gson.toJson(j);
+				}
+			}
+			catch(Exception e){
+				
+				e.printStackTrace();
+			}
 			
-			return;
+			json = "[" + json+ "]";
+            
+            // fw.close();
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST");
+            response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+            response.setHeader("Access-Control-Max-Age", "86400");
+            //Tell the browser what requests we allow.
+            response.setHeader("Allow", "GET, HEAD, POST, TRACE, OPTIONS");
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(new Gson().toJson(json)); 
+            System.out.println("Done");
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher("results.jsp");
+			request.setAttribute("json", json);
+			dispatcher.forward(request, response);
 		}
-	}
+	//}
+	
+	public String removeLastChar(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        return s.substring(0, s.length()-1);
+    }	
+	
 }
